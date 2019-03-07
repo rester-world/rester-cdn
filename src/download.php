fcc75e6a01ff1e81ac276281997e27b509ccba8e
박경종 <kevinpark@webace.co.kr>
2019-03-08 03:12:21 +0900

    다운로드 기능 추가

--- src/download.php
+++ src/download.php
@@ -1,17 +1,51 @@
 <?php
+try
+{
+    require_once './rester/common.php';
+
+    $extension = substr($_GET[QUERY_FILE],strrpos($_GET[QUERY_FILE],'.')+1);
+    if(preg_match('/^[a-z0-9-_]*$/i',strtolower($_GET[QUERY_MODULE]),$matches))
+    {
+        $module = $matches[0];
+    }
+    else
+    {
+        throw new Exception("모듈명 오류");
+    }
+
+    $file = base64_decode(urldecode(substr($_GET[QUERY_FILE],0,strrpos($_GET[QUERY_FILE],'.'))));
+    $file_path = '../files/'.$module.'/'.$file.'.'.$extension;
+    if(!is_file($file_path))
+    {
+        throw new Exception("파일이 없습니다.");
+    }
+    $file_name = urldecode(explode('_',$file)[1]);
+    $file_size = filesize($file_path);
+    $headers = [
+        "Pragma: public",
+        "Expires: 0",
+        "Content-Type: application/octet-stream",
+        "Content-Disposition: attachment; filename='$file_name'",
+        "Content-Transfer-Encoding: binary",
+        "Content-Length: $file_size"
+    ];
+    foreach ($headers as $v)
+    {
+        header($v);
+    }
+    echo file_get_contents($file_path);
+    exit;
+}
+catch (Exception $e)
+{
+    rester_response::error($e->getMessage());
+    rester_response::error_trace(explode("\n",$e->getTraceAsString()));
+}
+
+rester_response::run();
+
 
 $filename = $this->data['file_name'];
 $filesize = $this->data['file_size'];
 //$filepath = $this->get_uploaded_path();
 
-$headers = [
-    "Pragma: public",
-    "Expires: 0",
-    "Content-Type: application/octet-stream",
-    "Content-Disposition: attachment; filename='$filename'",
-    "Content-Transfer-Encoding: binary",
-    "Content-Length: $filesize"
-];
-rester::set_header($headers);
-
-return file_get_contents($filepath);
